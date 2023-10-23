07a58ca3e91b223c1c72cf114ce7fbaabe796492
Matias Perez <ma7iasperez@gmail.com>
2023-10-23 18:27:49 -0300

    Agregado de filtrado por nombre de cuidad y colores del mapa.

--- src/pages/Estadisticas.js
+++ src/pages/Estadisticas.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, TextInput} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import url from '../../constants/url';
 import axios from 'axios';
@@ -10,13 +10,13 @@ const Estadisticas = ({ navigation }) => {
   const [jwt, setJwt] = useState(null);
   const [iduser, setIduser] = useState(null);
   const [estadisticas, setEstadisticas] = useState([]);
+  const [filtroCiudad, setFiltroCiudad] = useState(''); 
 
   useEffect(() => {
     axios
       .get(url + 'api/ciudades', {})
       .then((response) => {
         setEstadisticas(response.data.data);
-        console.log(response.data.data);
       })
       .catch((error) => {
         console.error('Error al obtener estadísticas:', error);
@@ -38,6 +38,17 @@ const Estadisticas = ({ navigation }) => {
     } catch (e) {
       console.log(e);
     }
+  };
+  
+  const incidentColors = {
+    Robo: 'rgba(231, 76, 60, 0.65)', // Rojo para el tipo "Robo"
+    Hurto: 'rgba(52, 152, 219, 0.65)', // Azul para el tipo "Hurto"
+    'Asalto a propiedad': 'rgba(46, 204, 113, 0.65)', // Verde para el tipo "Asalto a propiedad"
+    Vandalismo: 'rgba(230, 126, 34, 0.65)', // Naranja para el tipo "Vandalismo"
+    'Delito sexual': 'rgba(186, 83, 158, 0.65)', // Violeta para el tipo "Delito sexual"
+    Incendio: 'rgba(240, 98, 146, 0.65)', // Rosa para el tipo "Incendio"
+    Saqueo: 'rgba(101, 198, 187, 0.65)', // Celeste para el tipo "Saqueo"
+    Otro: 'rgba(169, 169, 169, 0.65)', // Gris para el tipo "Otro"
   };
 
   const navegar = () => {
@@ -61,6 +72,11 @@ const Estadisticas = ({ navigation }) => {
 
   const groupedEstadisticas = groupEstadisticasByCiudad(estadisticas);
 
+  // Filtrar las ciudades que coinciden con la búsqueda
+  const ciudadesFiltradas = Object.keys(groupedEstadisticas).filter(
+    (ciudad) => ciudad.toLowerCase().includes(filtroCiudad.toLowerCase())
+  );
+
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.menuButton} onPress={navegar}>
@@ -71,14 +87,21 @@ const Estadisticas = ({ navigation }) => {
         Estadísticas de incidentes
       </Text>
 
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Buscar por nombre de ciudad"
+        value={filtroCiudad}
+        onChangeText={(text) => setFiltroCiudad(text)}
+      />
+
       <FlatList
         style={styles.flatContainer}
-        data={Object.keys(groupedEstadisticas)}
+        data={ciudadesFiltradas}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
           <View style={styles.ciudadContainer}>
             <Text style={styles.ciudadTitle}>Ciudad: {item}</Text>
-            
+
             {/* Gráfico de torta para cada localidad */}
             <PieChart
               data={
@@ -86,42 +109,42 @@ const Estadisticas = ({ navigation }) => {
                   {
                     name: 'Robo',
                     population: groupedEstadisticas[item].reduce((acc, estadistica) => acc + estadistica.Robo, 0),
-                    color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+                    color: incidentColors.Robo,
                   },
                   {
                     name: 'Asalto a propiedad',
                     population: groupedEstadisticas[item].reduce((acc, estadistica) => acc + estadistica.Asaltoapropiedad, 0),
-                    color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+                    color: incidentColors['Asalto a propiedad'],
                   },
                   {
                     name: 'Hurto',
                     population: groupedEstadisticas[item].reduce((acc, estadistica) => acc + estadistica.Hurto, 0),
-                    color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+                    color: incidentColors.Hurto,
                   },
                   {
                     name: 'Vandalismo',
                     population: groupedEstadisticas[item].reduce((acc, estadistica) => acc + estadistica.Vandalismo, 0),
-                    color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+                    color: incidentColors.Vandalismo,
                   },
                   {
                     name: 'Delito sexual',
                     population: groupedEstadisticas[item].reduce((acc, estadistica) => acc + estadistica.Delitosexual, 0),
-                    color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+                    color: incidentColors['Delito sexual'],
                   },
                   {
                     name: 'Incendio',
                     population: groupedEstadisticas[item].reduce((acc, estadistica) => acc + estadistica.Incendio, 0),
-                    color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+                    color: incidentColors.Incendio,
                   },
                   {
                     name: 'Saqueo',
                     population: groupedEstadisticas[item].reduce((acc, estadistica) => acc + estadistica.Saqueo, 0),
-                    color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+                    color: incidentColors.Saqueo,
                   },
                   {
                     name: 'Otro',
                     population: groupedEstadisticas[item].reduce((acc, estadistica) => acc + estadistica.Otro, 0),
-                    color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+                    color: incidentColors.Otro,
                   },
                 ]
               }
@@ -188,6 +211,15 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     borderRadius: 4,
     padding: 5,
+    marginBottom: 10,
+  },
+  searchInput: {
+    width: '90%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
     marginBottom: 10,
   },
 });
